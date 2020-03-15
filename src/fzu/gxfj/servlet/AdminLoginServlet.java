5c7cf4d224570c8a686e708ffb0c1f6c19e405f7
爱学习的水先生 <60643156+shuiYe-704265085@users.noreply.github.com>
2020-03-15 16:55:43 +0800

    增加了AdminLoginServlet,AdminSetServlet,AdminListWinServlet

--- src/fzu/gxfj/servlet/AdminLoginServlet.java
+++ src/fzu/gxfj/servlet/AdminLoginServlet.java
@@ -1,4 +1,64 @@
 package fzu.gxfj.servlet;
 
-public class AdminLoginServlet {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fzu.gxfj.dao.AdminDAO;
+import fzu.gxfj.pojo.Admin;
+
+/**
+ * Servlet implementation class AdminLoginServlet
+ */
+@WebServlet("/adminLoginServlet")
+public class AdminLoginServlet extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminLoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String account = request.getParameter("account");
+		String password = request.getParameter("password");
+		if(account != null && password != null) {
+			AdminDAO adminDAO = new AdminDAO();
+			Admin admin = adminDAO.getAdminUser(account);
+			if(admin != null && admin.getPassword().equals(password))
+				request.getRequestDispatcher("Setup.jsp").forward(request, response);
+			request.setAttribute("hint", "没有此用户");//返回提示信息
+			request.getRequestDispatcher("adminlogin.jsp").forward(request, response);
+		}
+		else {
+			request.setAttribute("hint", "用户名和密码不能为空");//返回提示信息
+			request.getRequestDispatcher("adminlogin.jsp").forward(request, response);
+		}
+			
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	
+
 }
