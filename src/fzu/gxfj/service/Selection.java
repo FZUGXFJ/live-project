384f7ae634ef96e485122ed9d3a763c83d8f63f4
herokilito <herokilito@outlook.com>
2020-03-15 13:05:31 +0800

    加入数据库连接util

---
+++ src/fzu/gxfj/service/Selection.java
@@ -0,0 +1,15 @@
+package fzu.gxfj.service;
+
+public class Selection {
+
+    /**
+     * 抽选预约者
+     */
+    public void select() {
+
+    }
+
+    private void GeneratePurchaseVoucher() {
+
+    }
+}
