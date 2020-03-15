bd93f3ae5566df9fe1bb5bea177af4918b15a0b5
QMBX <30679822+QMBX@users.noreply.github.com>
2020-03-15 14:20:30 +0800

    修复一些小错误

--- src/fzu/gxfj/dao/AdminDao.java
+++ src/fzu/gxfj/dao/AdminDao.java
@@ -1,5 +1,11 @@
 package fzu.gxfj.dao;
 
+import fzu.gxfj.pojo.Admin;
+
 public class AdminDao {
-	public Admin getAdminUser(String account);//通过管理员账户获取管理员信息，无则返回空
+	public Admin getAdminUser(String account) {
+		return null;
+	}
+
+	//通过管理员账户获取管理员信息，无则返回空
 }
