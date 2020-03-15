5fb5a36161a29fd93bb0cce7fb140eec541dda7c
ReckyZzz <244373762@qq.com>
2020-03-15 14:19:44 +0800

    合并

--- src/fzu/gxfj/dao/AppointmentDAO.java
+++ src/fzu/gxfj/dao/AppointmentDAO.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class AppointmentDAO {
 	//通过场次id获取制定场次中奖名单，筛选isWin=1
-	public ArrayList<Appointment> getWin(int id); 
+	public ArrayList<Appointment> getWin(int id);
 
     public List<Appointment> listAppointedThisTurn() {
         return null;
